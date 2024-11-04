65dd8b6c07702fdda5e3f8b8605377b552113811
cooper <cshapard@smu.edu>
2024-11-04 15:57:26 -0600

    added __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from classes.kb import KB, NGramData
+from .crossover import breed
+from .fitness import evaluate_population
+from .mutation import mutate
+from .population import initialize_population, update_population
